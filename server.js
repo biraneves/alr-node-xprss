38744edef79197249e37f1a5d041eb57c205bec6
Ubirajara Neves <ubirajara.neves@usp.br>
2022-12-23 10:03:12 -0300

    Basic use of routes.

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ import http from 'http';
 
 const port = 3000;
 
+const routes = {
+    '/': 'Node.js course',
+    '/books': 'Page of books',
+    '/authors': 'List of authors',
+    '/editors': 'Page of editors',
+    '/about': 'Project infos',
+};
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Node.js course');
+    res.end(routes[req.url]);
 });
 
 server.listen(port, () => console.log(`Listening on http://localhost:${port}`));
